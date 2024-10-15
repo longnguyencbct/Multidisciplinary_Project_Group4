a7e154bc4e190d25536399b10390b0fa5e597129
Trương Huỳnh Anh Dũng <150664748+vodanh138@users.noreply.github.com>
2024-10-16 05:38:40 +0700

    16/10/2024

--- Demo_CS/demo.cpp
+++ Demo_CS/demo.cpp
@@ -7,7 +7,6 @@
 #include <string>
 
 using namespace std;
-
 void print2DMatrix(const vector<vector<double>>& matrix) {
     for (const auto& row : matrix) {
         for (const auto& value : row) {
@@ -27,30 +26,67 @@ void printBit(const vector<bitset<32>>& packed_data) {
         cout << bs << endl;
     }
 }
-
-vector<bitset<32>> bitPack(const vector<double>& errors) {
+vector<bitset<32>> bitPack1(const vector<double>& errors, string row) {
     vector<bitset<32>> packed;
-    for (double err : errors) {
-        bool negative = false;
-        if (err < 0) {
-            negative = true;
-            err = -err;
+    string name = "encode";
+    string nameDec = "encode";
+    name = name + row + "(binary form).txt";
+    nameDec = nameDec + row + "(decimal form).txt";
+    cout << name << endl;
+    cout << nameDec << endl;
+    ofstream outFile(name);
+    ofstream outFileDec(nameDec);
+    if (outFile.is_open() && outFileDec.is_open()) {
+        for (double err : errors) {
+            bool negative = false;
+            if (err < 0) {
+                negative = true;
+                err = -err;
+            }
+            bitset<32> errBit = bitset<32>(err);
+            errBit = errBit << 1;
+            if (negative)
+                errBit |= bitset<32>(1);
+            // This last bit = 1 mean negative and 0 is positive
+            packed.push_back(bitset<32>(errBit));
+            outFileDec << errBit << endl;
+            outFile.write(reinterpret_cast<const char*>(&errBit), sizeof(errBit));
         }
-        bitset<32> errBit = bitset<32>(err);
-        errBit = errBit << 1;
-        if (negative) 
-            errBit |= bitset<32>(1);
-        // This last bit = 1 mean negative and 0 is positive
-        packed.push_back(bitset<32>(errBit));
+        outFile.close();
+        outFileDec.close();
+        cout << "yes" << endl;
+    }
+    else {
+        cerr << "no" << endl;
     }
     return packed;
 }
 
-vector<vector<double>> readCSV(const string& filename) {
+vector<vector<double>> readCSV(const string& filename, int& count) {
     vector<vector<double>> data;
     ifstream file(filename);
     string line;
     getline(file, line);
+
+    if (getline(file, line)) {
+        stringstream ss(line);
+        string value,timestamp;
+        vector<double> row;
+        getline(ss, timestamp, ',');
+        while (getline(ss, value, ',')) {
+            double number = stod(value);
+            double rounded_number;
+            if (number * 1000.0 >= (int)(number * 1000.0) + 0.5) {
+                rounded_number = (int)(number * 1000.0 + 1) / 1000.0;
+            }
+            else
+                rounded_number = (int)(number * 1000.0) / 1000.0;
+            row.push_back(rounded_number);
+            count++;
+        }
+        data.push_back(row);
+    }
+
     while (getline(file, line)) {
         stringstream ss(line);
         string timestamp, value;
@@ -58,7 +94,12 @@ vector<vector<double>> readCSV(const string& filename) {
         getline(ss, timestamp, ',');
         while (getline(ss, value, ',')) {
             double number = stod(value);
-            double rounded_number = (int)(number * 1000.0) / 1000.0;
+            double rounded_number;
+            if (number * 1000.0 >= (int)(number * 1000.0) + 0.5) {
+                rounded_number = (int)(number * 1000.0 + 1) / 1000.0;
+            }
+            else
+                rounded_number = (int)(number * 1000.0) / 1000.0;
             row.push_back(rounded_number);
         }
         data.push_back(row);
@@ -66,7 +107,7 @@ vector<vector<double>> readCSV(const string& filename) {
     return data;
 }
 
-vector<bitset<32>> encodeBlock(const vector<double>& data, vector<double>&errors) {
+vector<bitset<32>> encodeBlock(const vector<double>& data, vector<double>&errors, string row) {
     errors.push_back(data[0]);
     double prev_value = data[0];
     for (size_t i = 1; i < data.size(); i++) {
@@ -77,25 +118,38 @@ vector<bitset<32>> encodeBlock(const vector<double>& data, vector<double>&errors
     }
     //Show first row of input after encode (in decimal)
     //printMatrix(errors);
-    vector<bitset<32>> packed_data = bitPack(errors);
+    vector<bitset<32>> packed_data = bitPack1(errors, row);
     return packed_data;
 }
 
-vector<double> decode(const vector<bitset<32>>& packed_data, vector<double>& errors) {
+vector<double> decode(const vector<bitset<32>>& packed_data, vector<double>& errors, string row) {
+    string name = "decode";
+    name = name + row + ".txt";
+    cout << name << endl;
+    ofstream outFile(name);
     vector<double> decoded_data;
     double prev_value = 0;
-    for (const auto& packed : packed_data) {
-        bool negative = false;
-        if (packed[0] == 1)
-            negative = true;
-        bitset<32> errorBit = packed >> 1;
-        double error = errorBit.to_ulong();
-        if (negative)
-            error = -error;
-        errors.push_back(error);
-        double original_value = prev_value + error;
-        decoded_data.push_back(original_value/1000);
-        prev_value = original_value;
+    if (outFile.is_open()) {
+        for (const auto& packed : packed_data) {
+            bool negative = false;
+            if (packed[0] == 1)
+                negative = true;
+            bitset<32> errorBit = packed >> 1;
+            double error = errorBit.to_ulong();
+            if (negative)
+                error = -error;
+            errors.push_back(error);
+            double original_value = prev_value + error;
+            decoded_data.push_back(original_value / 1000);
+            double outputValue = original_value / 1000;
+            outFile << original_value / 1000 << endl;
+            prev_value = original_value;
+        }
+        outFile.close();
+        cout << "yes" << endl;
+    }
+    else {
+        cerr << "no" << endl;
     }
     return decoded_data;
 }
@@ -104,32 +158,30 @@ vector<double> decode(const vector<bitset<32>>& packed_data, vector<double>& err
 
 int main() {
     string filename = "air_quality_monitors.csv";
-    vector<vector<double>> csv_data = readCSV(filename);
+    int count = 0;
+    vector<vector<double>> csv_data = readCSV(filename,count);
     //Show input rounded to 3 decimal 
     //print2DMatrix(csv_data);
 
-    vector<double> data;
-    for (const auto& row : csv_data) {
-        data.push_back((int)(row[0]*1000));
+    for (int i = 0; i < count; i++) {
+        vector<double> data;
+        for (const auto& row : csv_data) {
+            data.push_back((int)(row[i] * 1000));
+        }
+        //Show first row of input and *1000 so no decimal
+        //printMatrix(data);
+
+        vector<double> encodedError;
+        string row = to_string(i + 1);
+        vector<bitset<32>> encoded = encodeBlock(data, encodedError, row);
+        //Show first row of input after encode (in binary)
+        //printBit(encoded);
+
+        vector<double> decodedErrors;
+        vector<double> decoded = decode(encoded, decodedErrors,row);
+        //Show first row of input after encode then decode
+        //printMatrix(decoded);
+
     }
-    //Show first row of input and *1000 so no decimal
-    //printMatrix(data);
-
-    vector<double> encodedError;
-    vector<bitset<32>> encoded = encodeBlock(data, encodedError);
-    //Show first row of input after encode (in binary)
-    //printBit(encoded);
-
-    vector<double> decodedErrors;
-    vector<double> decoded = decode(encoded, decodedErrors);
-    //Show first row of input after encode then decode
-    //printMatrix(decoded);
-
-    for (int i = 0; i < decoded.size(); i++) {
-        if (data[i] != decoded[i] * 1000)
-            cout << "i = " << i << ": " << data[i] << " " << decoded[i] * 1000 << endl;
-        //cout << data[i] << " " << decoded[i] << endl;
-    }
-
     return 0;
 }
