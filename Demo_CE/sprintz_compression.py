18232cabb007d44f59107c00abe8b625530f6761
longnguyencbct <clongnguynvn@gmail.com>
2024-10-19 10:19:39 +0700

    Simplify pulsar function

--- Demo_CE/sprintz_compression.py
+++ Demo_CE/sprintz_compression.py
@@ -5,10 +5,4 @@ class SprintZCompression(Function):
         pass
 
     def process(self, input, context):
-        # Check if context is None
-        if context is not None:
-            logger = context.get_logger()
-            logger.info(f"Received message: {input}")
-        else:
-            print(f"Received message (No logger): {input}")
-        return input  # Just pass through for now
+        return input + '!' 
