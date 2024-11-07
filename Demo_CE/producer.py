ac66bc9f416eec07de8bf64fde399477f401a359
longnguyencbct <clongnguynvn@gmail.com>
2024-11-07 14:27:32 +0700

    Demo_CE Folder Cleaning

--- Demo_CE/producer.py
+++
@@ -1,8 +0,0 @@
-with open('server_address.txt', 'r') as file:
-    pulsar_address = file.readline().strip()  # Read the first line and strip any extra whitespace
-
-import pulsar
-client = pulsar.Client(pulsar_address)
-producer = client.create_producer('persistent://conference/ps/first')
-producer.send(('Hello, World!').encode('utf-8'))
-client.close()
--- Demo_CE/sprintz_compression.py
+++
@@ -1,8 +0,0 @@
-from pulsar import Function
-
-class SprintZCompression(Function):
-    def __init__(self):
-        pass
-
-    def process(self, input, context):
-        return input + '!' 
