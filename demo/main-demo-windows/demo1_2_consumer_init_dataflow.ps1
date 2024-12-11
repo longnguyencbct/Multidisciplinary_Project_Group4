3242a33e8a75ce99a9b90e566ea20078ec4118cb
cbct <clongnguynvn@gmail.com>
2024-12-11 16:07:37 +0700

    updated windows demo

--- demo/main-demo-windows/demo1_2_consumer_init_dataflow.ps1
+++ demo/main-demo-windows/demo1_2_consumer_init_dataflow.ps1
@@ -1,18 +1,21 @@
-# Function to handle termination
-function Terminate {
-    Write-Output "Terminating processes..."
-    docker exec -it init-setup /bin/bash -c "pkill -f air_quality_monitor_consumer.py"
-    exit
-}
+# # Function to handle termination
+# function Terminate {
+#     Write-Output "Terminating processes..."
+#     docker exec -it init-setup /bin/bash -c "pkill -f air_quality_monitor_consumer.py"
+#     exit
+# }
 
-# Trap termination signals
-Register-EngineEvent PowerShell.Exiting -Action { Terminate }
+# # Trap termination signals
+# Register-EngineEvent PowerShell.Exiting -Action { Terminate }
 
-# Terminate any existing consumer processes inside the container
-docker exec -it init-setup /bin/bash -c "pkill -f air_quality_monitor_consumer.py"
+# # Terminate any existing consumer processes inside the container
+# docker exec -it init-setup /bin/bash -c "pkill -f air_quality_monitor_consumer.py"
 
-# Start consumer in a new terminal
-Start-Process powershell -ArgumentList '-NoExit', '-Command', "docker exec -it init-setup /bin/bash -c 'cd /app/Demo_CE/Topics/Air_Quality_Monitor && python3 air_quality_monitor_consumer.py'"
+# # Start consumer in a new terminal
+# Start-Process powershell -ArgumentList '-NoExit', '-Command', "docker exec -it init-setup /bin/bash -c 'cd /app/Demo_CE/Topics/Air_Quality_Monitor && python3 air_quality_monitor_consumer.py'"
 
-# Wait indefinitely
-Start-Sleep -Seconds ([int]::MaxValue)
+# # Wait indefinitely
+# Start-Sleep -Seconds ([int]::MaxValue)
+
+.\clear_prev_data.ps1
+.\demo2_1_consumer_rerun.ps1
