6fbbf461c96bc2d90c0c2f687aa96b8658120731
longnguyencbct <clongnguynvn@gmail.com>
2024-12-11 10:40:14 +0700

    test: air quality monitor failover

--- Demo_CE/Topics/Air_Quality_Monitor/air_quality_monitor_consumer.py
+++ Demo_CE/Topics/Air_Quality_Monitor/air_quality_monitor_consumer.py
@@ -17,8 +17,7 @@ with open('../pulsar_address.txt', 'r') as file:
     pulsar_address = file.readline().strip()
 
 client = pulsar.Client(pulsar_address)
-consumer = client.subscribe('persistent://Smart_Home/Environmental_Monitoring/Air_Quality_Monitor', subscription_name='subscription')
-
+consumer = client.subscribe('persistent://Smart_Home/Environmental_Monitoring/Air_Quality_Monitor', subscription_name='subscription', consumer_type=pulsar.ConsumerType.Failover)
 prev_row = ""
 
 # Load previous row from file if it exists
